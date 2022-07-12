b6c430dfdc527d02adf853164d6a9c5d33547dfa
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-07-12 17:08:24 +0200

    lint + removing extracting zip

--- notion-backup.js
+++ notion-backup.js
@@ -1,24 +1,23 @@
 #!/usr/bin/env node
+/* eslint-disable quotes */
 /* eslint no-await-in-loop: 0 */
 
-let axios = require('axios')
-  , extract = require('extract-zip')
-  , { retry } = require('async')
-  , { createWriteStream, mkdirSync, rmdirSync } = require('fs')
-  , { join } = require('path')
-  , notionAPI = 'https://www.notion.so/api/v3'
-  , { NOTION_TOKEN, NOTION_SPACE_ID } = process.env
-  , client = axios.create({
-      baseURL: notionAPI,
-      headers: {
-        Cookie: `token_v2=${NOTION_TOKEN}`
-      },
-    })
-  , die = (str) => {
-      console.error(str);
-      process.exit(1);
-    }
-;
+let axios = require("axios"),
+  { retry } = require("async"),
+  { createWriteStream, mkdirSync, rmdirSync } = require("fs"),
+  { join } = require("path"),
+  notionAPI = "https://www.notion.so/api/v3",
+  { NOTION_TOKEN, NOTION_SPACE_ID } = process.env,
+  client = axios.create({
+    baseURL: notionAPI,
+    headers: {
+      Cookie: `token_v2=${NOTION_TOKEN}`,
+    },
+  }),
+  die = (str) => {
+    console.error(str);
+    process.exit(1);
+  };
 
 if (!NOTION_TOKEN || !NOTION_SPACE_ID) {
   die(`Need to have both NOTION_TOKEN and NOTION_SPACE_ID defined in the environment.
@@ -26,45 +25,46 @@ See https://medium.com/@arturburtsev/automated-notion-backups-f6af4edc298d for
 notes on how to get that information.`);
 }
 
-async function post (endpoint, data) {
+async function post(endpoint, data) {
   return client.post(endpoint, data);
 }
 
-async function sleep (seconds) {
+async function sleep(seconds) {
   return new Promise((resolve) => {
     setTimeout(resolve, seconds * 1000);
   });
 }
 
 // formats: markdown, html
-async function exportFromNotion (format) {
+async function exportFromNotion(format) {
   try {
-    let { data: { taskId } } = await post('enqueueTask', {
+    let {
+      data: { taskId },
+    } = await post("enqueueTask", {
       task: {
-        eventName: 'exportSpace',
+        eventName: "exportSpace",
         request: {
           spaceId: NOTION_SPACE_ID,
           exportOptions: {
             exportType: format,
-            timeZone: 'America/New_York',
-            locale: 'en',
+            timeZone: "Europe/Warsaw",
+            locale: "pl",
           },
         },
       },
     });
     console.warn(`Enqueued task ${taskId}`);
-    let failCount = 0
-      , exportURL
-    ;
+    let failCount = 0,
+      exportURL;
     while (true) {
       if (failCount >= 5) break;
       await sleep(10);
-      let { data: { results: tasks } } = await retry(
-        { times: 3, interval: 2000 },
-        async () => post('getTasks', { taskIds: [taskId] })
+      let {
+        data: { results: tasks },
+      } = await retry({ times: 3, interval: 2000 }, async () =>
+        post("getTasks", { taskIds: [taskId] })
       );
-      let task = tasks.find(t => t.id === taskId);
-      // console.warn(JSON.stringify(task, null, 2)); // DBG
+      let task = tasks.find((t) => t.id === taskId);
       if (!task) {
         failCount++;
         console.warn(`No task, waiting.`);
@@ -72,51 +72,53 @@ async function exportFromNotion (format) {
       }
       if (!task.status) {
         failCount++;
-        console.warn(`No task status, waiting. Task was:\n${JSON.stringify(task, null, 2)}`);
+        console.warn(
+          `No task status, waiting. Task was:\n${JSON.stringify(task, null, 2)}`
+        );
         continue;
       }
-      if (task.state === 'in_progress') console.warn(`Pages exported: ${task.status.pagesExported}`);
-      if (task.state === 'failure') {
+      if (task.state === "in_progress") {
+        console.warn(`Pages exported: ${task.status.pagesExported}`);
+      }
+      if (task.state === "failure") {
         failCount++;
         console.warn(`Task error: ${task.error}`);
         continue;
       }
-      if (task.state === 'success') {
+      if (task.state === "success") {
         exportURL = task.status.exportURL;
         break;
       }
     }
     let res = await client({
-      method: 'GET',
+      method: "GET",
       url: exportURL,
-      responseType: 'stream'
+      responseType: "stream",
     });
-    let stream = res.data.pipe(createWriteStream(join(process.cwd(), `${format}.zip`)));
+    let stream = res.data.pipe(
+      createWriteStream(join(process.cwd(), `${format}.zip`))
+    );
     await new Promise((resolve, reject) => {
-      stream.on('close', resolve);
-      stream.on('error', reject);
+      stream.on("close", resolve);
+      stream.on("error", reject);
     });
-  }
-  catch (err) {
+  } catch (err) {
     die(err);
   }
 }
 
-async function run () {
-  let cwd = process.cwd()
-    , mdDir = join(cwd, 'markdown')
-    , mdFile = join(cwd, 'markdown.zip')
-    , htmlDir = join(cwd, 'html')
-    , htmlFile = join(cwd, 'html.zip')
-  ;
-  await exportFromNotion('markdown');
+async function run() {
+  let cwd = process.cwd(),
+    mdDir = join(cwd, "markdown"),
+    mdFile = join(cwd, "markdown.zip"),
+    htmlDir = join(cwd, "html"),
+    htmlFile = join(cwd, "html.zip");
+  await exportFromNotion("markdown");
   rmdirSync(mdDir, { recursive: true });
   mkdirSync(mdDir, { recursive: true });
-  await extract(mdFile, { dir: mdDir });
-  await exportFromNotion('html');
+  await exportFromNotion("html");
   rmdirSync(htmlDir, { recursive: true });
   mkdirSync(htmlDir, { recursive: true });
-  await extract(htmlFile, { dir: htmlDir });
 }
 
 run();
